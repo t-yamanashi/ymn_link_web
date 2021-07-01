291bd2c2c119e1e1c0af4320ee4dacd18c934234
user <user@penguin>
2021-07-01 14:37:34 +0900

    tty add

---
+++ lib/tty.ex
@@ -0,0 +1,22 @@
+defmodule Tty do
+  @moduledoc """
+  Documentation for `TTY`.
+  """
+
+  @doc """
+  Send command.
+
+  ## Examples
+
+  """
+  def send(cmd) do
+    {:ok, pid} = Circuits.UART.start_link
+    Circuits.UART.open(pid, "/dev/ttyUSB0", speed: 115200, active: false)
+    Circuits.UART.write(pid, cmd)
+    :timer.sleep(100)
+    {:ok, ret} = Circuits.UART.read(pid, 10)
+    Circuits.UART.close(pid)
+    ret
+  end
+end
+
