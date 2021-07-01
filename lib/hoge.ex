1544bb6437cb04125303bbf4ad02ee57cd2c1e8c
user <user@penguin>
2021-07-01 10:22:29 +0900

    test

---
+++ lib/hoge.ex
@@ -0,0 +1,18 @@
+defmodule Hoge do
+  @moduledoc """
+  Documentation for `Hoge`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Hoge.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
