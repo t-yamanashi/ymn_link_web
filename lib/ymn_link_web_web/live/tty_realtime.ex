41f55ba4be8e629ff964d1110df56aeab2ede7df
ymn <ae86bg5@yahoo.co.jp>
2021-07-09 08:29:50 +0900

    liveview

--- lib/ymn_link_web_web/live/tty_realtime.ex
+++ lib/ymn_link_web_web/live/tty_realtime.ex
@@ -3,25 +3,18 @@ defmodule YmnLinkWebWeb.TtyRealtime do
 
   @impl true
   def mount(_params, _session, socket) do
+    if connected?(socket) do
+      Process.send_after(self(), :tick, 50)
+    end
     { :ok, assign(socket, results: [])}
   end
 
   @impl true
-  def handle_event("change", %{"v" => v }, socket ) do
-    send(self(), {:submit, v})
-    {:noreply, assign(socket, v: v)}
-  end
-
-  @impl true
-  def handle_info( {:submit, v}, socket ) do
+  def handle_info(:tick, socket ) do
+    Process.send_after(self(), :tick, 50)
     t = Tty.send("z;")
-    results = [%{abc: t}, %{abc: DateTime.utc_now}]
+    results = [%{value: t}, %{value: DateTime.utc_now}]
     { :noreply, assign(socket, results: results)}
   end
 
-  @imple true
-  def handle_event("hoge", %{"v" => v}, socket ) do
-    send(self(), {:submit, v})
-    {:noreply, assign(socket, v: v)}
-  end
 end
