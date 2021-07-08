5afcd835e9ee4d6087f88502e7e500045b430641
ymn <ae86bg5@yahoo.co.jp>
2021-07-09 07:29:09 +0900

    liveview

--- lib/ymn_link_web_web/live/tty_realtime.ex
+++ lib/ymn_link_web_web/live/tty_realtime.ex
@@ -1,39 +1,27 @@
 defmodule YmnLinkWebWeb.TtyRealtime do
-  use Phoenix.LiveView
+  use YmnLinkWebWeb, :live_view
 
-  def render(assigns) do
-      ~L"""
-        <script>
-          function st(){
-            var hoge = document.getElementById("hoge");
-            hoge.click();
-            setTimeout("st()", 100);
-          }
-        </script>
 
-         <form>
-         <input type="button" phx-hook="Hoge" id="hoge" style="display:none" >
-         <input type="button"  value="Start" onclick="st()">
-        </form>
-        <%= for result <- @results do %>
-          <%= result[:abc] %><br>
-        <% end %>
-    """
-  end
-
+  @impl true
   def mount(_params, _session, socket) do
     { :ok, assign(socket, results: [])}
   end
+
+
+  @impl true
   def handle_event("change", %{"v" => v }, socket ) do
     send(self(), {:submit, v})
     {:noreply, assign(socket, v: v)}
   end
+
+  @impl true
   def handle_info( {:submit, v}, socket ) do
     t = Tty.send("z;")
     results = [%{abc: t}, %{abc: DateTime.utc_now}]
     { :noreply, assign(socket, results: results)}
   end
 
+  @imple true
   def handle_event("hoge", %{"v" => v}, socket ) do
     send(self(), {:submit, v})
     {:noreply, assign(socket, v: v)}
