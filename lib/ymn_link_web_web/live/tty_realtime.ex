34b1a5a4c7edcd5635f2e24b2e16c91858e21425
ymn <ae86bg5@yahoo.co.jp>
2021-07-09 15:45:13 +0900

    liveview

--- lib/ymn_link_web_web/live/tty_realtime.ex
+++ lib/ymn_link_web_web/live/tty_realtime.ex
@@ -14,8 +14,24 @@ defmodule YmnLinkWebWeb.TtyRealtime do
     Process.send_after(self(), :tick, 50)
     results = Tty.send("z;")
               |> String.split(",")
-              |> Enum.map(fn(x) -> %{value: x} end)
+              |> Enum.map(fn(x) -> create_data(x) end)
     { :noreply, assign(socket, results: results)}
   end
 
+  def create_data(x) do
+    {val, _} = Integer.parse(x)
+    width = div(val, 3)
+    %{value: x, 
+      style: "width:" <> Integer.to_string(width) 
+      <>  "px;background-color:" <> create_color(val) <> ";font-size:40px;"
+    }
+  end 
+
+  def create_color(val) do
+    cond do
+      val < 600 -> "#ccccff"
+      val > 600 && val < 800 -> "#ffffcc" 
+      true -> "#ffcccc"  
+    end
+  end
 end
